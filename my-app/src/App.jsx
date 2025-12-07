becd58c2c179fec112670cfefb19a9f9b7ba10bb
pepper <peppermaryparay@gmail.com>
2025-12-07 15:29:02 -0600

    trying to straighten out my mistakes. sorry for the delay.

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { createRoot } from 'react-dom/client'
 import './App.css'
+import './about.html'
+import './story.html'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -24,6 +27,23 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        <br>
+        <div>
+          function NavigationBar() {
+  // TODO: Actually implement a navigation bar
+  return <h1>Hello from React!</h1>;
+import { createRoot } from 'react-dom/client';
+
+function Hello() {
+  return (i
+    <h1>Hello World!</h1>
+  );
+}
+
+createRoot(document.getElementById('root')).render(
+  <Hello />
+);
+</div>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
@@ -33,3 +53,4 @@ function App() {
 }
 
 export default App
+
